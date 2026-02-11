cc3275809e1b30d5c2bd228574b66af1754f6bda
Bakraoui <ismail.bakraoui@emsi-edu.ma>
2026-02-11 04:00:17 +0100

    backend/app/models: update user.py  model adjustments

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, Boolean
 from app.database import Base
 
+
 class User(Base):
     __tablename__ = "users"
 
@@ -10,3 +11,4 @@ class User(Base):
     hashed_password = Column(String(255))
     is_active = Column(Boolean, default=True)
     role = Column(String(50), default="doctor")
+    is_admin = Column(Boolean, default=False)
