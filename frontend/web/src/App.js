846687b0890d6d50b94f9ed8bca80ddc6124c870
Bakraoui <ismail.bakraoui@emsi-edu.ma>
2026-02-11 04:00:17 +0100

    frontend/web/src: update App.js  app bootstrap changes

--- frontend/web/src/App.js
+++ frontend/web/src/App.js
@@ -4,6 +4,11 @@ import { ToastProvider } from './components/common';
 import LoginPage from './pages/Login';
 import SignupPage from './pages/Signup';
 import DashboardPage from './pages/Dashboard';
+import AdminDashboard from './pages/Admin/AdminDashboard';
+import AdminUsers from './pages/Admin/AdminUsers';
+import AdminAnalytics from './pages/Admin/AdminAnalytics';
+import AdminSecurity from './pages/Admin/AdminSecurity';
+import AdminSettings from './pages/Admin/AdminSettings';
 import PatientPage from './pages/Patients';
 import PatientDetailPage from './pages/Patients/PatientDetailPage';
 import AppointmentsPage from './pages/Appointments';
@@ -22,6 +27,11 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin/users" element={<AdminUsers />} />
+        <Route path="/admin/analytics" element={<AdminAnalytics />} />
+        <Route path="/admin/security" element={<AdminSecurity />} />
+        <Route path="/admin/settings" element={<AdminSettings />} />
         <Route path="/patients" element={<PatientPage />} />
         <Route path="/patients/:id" element={<PatientDetailPage />} />
         <Route path="/appointments" element={<AppointmentsPage />} />
